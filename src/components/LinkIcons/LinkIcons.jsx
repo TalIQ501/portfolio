4f8fef5f8a019a65c5976b334bb3382de302155a
TalIq501 <talhaiqbal501@gmail.com>
2024-10-28 10:16:52 +0500

    Rested public with res

--- src/components/LinkIcons/LinkIcons.jsx
+++ src/components/LinkIcons/LinkIcons.jsx
@@ -4,8 +4,8 @@ const LinkIcons = () => {
     return (
         <div className={styles.linkIcons}>
             <div className={styles.container}>
-                <a href='https://linkedin.com/in/talha-iqbal-80b91523b'><img src='/img/lang-img/LinkedIn Logo.svg' /></a>
-                <a href='https://github.com/TalIQ501'><img src='/img/lang-img/GitHub Logo.svg' /></a>
+                <a href='https://linkedin.com/in/talha-iqbal-80b91523b'><img src='/public/img/lang-img/LinkedIn Logo.svg' /></a>
+                <a href='https://github.com/TalIQ501'><img src='/public/img/lang-img/GitHub Logo.svg' /></a>
             </div>
         </div>
     );
